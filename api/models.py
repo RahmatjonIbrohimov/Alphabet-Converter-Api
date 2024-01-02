7a836d2843eb0020343b66eb7afe946b2e360463
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-02 19:56:59 +0500

    Added Convert MOdel

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class ConvertTextModel(models.Model):
+    text = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.text[0:16]} ...'
+
+
+class ConvertFileModel(models.Model):
+    file = models.FileField(upload_to='files/', blank=True)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.file.name}'
