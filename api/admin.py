bce1f09473ba4251860229d75e2a9b7a64148b56
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-02 19:41:30 +0500

    update admin, added COnvertFileModel

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import ConvertModel
+from .models import ConvertTextModel, ConvertFileModel
 
 
 # Register your models here.
-admin.site.register(ConvertModel)
+admin.site.register(ConvertTextModel)
+admin.site.register(ConvertFileModel)
