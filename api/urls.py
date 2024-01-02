ab524713e536b44049d371a2d62aeec923dd3770
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-02 19:57:52 +0500

    Added some urls, add/

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,1 @@
-from django.urls import pathfrom . import viewsurlpatterns = [    path('', views.home)]
+from django.urls import pathfrom . import viewsurlpatterns = [    path('', views.home),    path('api/', views.HomeViews.as_view(), name='api'),    path('api/text/add/', views.AddTextView.as_view(), name='add-text'),    path('api/file/add/', views.AddFileView.as_view(), name='add-file'),]
