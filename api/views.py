4c20dafdf71622db845396423e3e175344c96edf
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-05 09:18:38 +0500

    added OpenFIleViews

--- api/views.py
+++ api/views.py
@@ -48,3 +48,73 @@ class LatinOrCyrillicView(APIView):
             return Response({'result': "Siz konvert qilishda adashdingiz!"})
 
 
+class OpenFileView(APIView):
+    def get(self, request, *args, **kwargs):
+        last_text = ConvertFileModel.objects.last().file.name
+        to = ConvertFileModel.objects.last().to
+
+        if last_text[-4:] == 'docx':
+            # print('Docx ga kirildi!')
+            fayl = last_text
+            document = Document(fayl)
+            res = """"""
+            for paragraph in document.paragraphs:
+                res += paragraph.text
+            try:
+                if to == "to_Latin":
+                    convert = to_latin(res)
+
+                    for i in range(len(document.paragraphs)):
+                        document.paragraphs[i].clear()
+                    document.add_paragraph(convert)
+                    document.save(last_text)
+                    return Response({'Original Text': f"http://127.0.0.1:8000/{last_text}", 'Cyrillic to Latin': convert})
+
+                elif to == 'to_Cyrillic':
+                    convert = to_ciril(res)
+                    # print(convert)
+
+                    for i in range(len(document.paragraphs)):
+                        document.paragraphs[i].clear()
+
+                    document.add_paragraph(convert)
+                    document.save(last_text)
+                    return Response({'Original Text': f"http://127.0.0.1:8000/{last_text}", 'Latin to Cyrillic': convert})
+
+            except KeyError:  # KeyError
+                return Response({'result': "Siz konvert qilishda adashdingiz! Yoki Alfabetda mavjud bo'lmagan belgi "
+                                           "kiritilgan!"})
+
+        else:
+            print('Start!')
+            with open(last_text, 'r+', encoding='utf-8') as fayl:
+                text = fayl.readlines()
+                # print(text)
+                for i in text:
+                    res = i.strip()
+                    # print(res)
+            try:
+                print('Kirildi!')
+                if to == "to_Latin":
+                    convert = to_latin(res)
+                    # print(convert)
+                    print(last_text)
+
+                    with open(last_text, 'w+', encoding='utf-8') as fayl:
+                        for convert in fayl:
+                            fayl.writelines(convert)
+                            print(fayl)
+
+                    return Response({'Original Text':  f"http://127.0.0.1:8000/{last_text}", 'Cyrillic to Latin': convert})
+
+                elif to == 'to_Cyrillic':
+                    convert = to_ciril(res)
+                    with open(last_text, 'w+', encoding='utf-8') as fayl:
+                        for convert in fayl:
+                            fayl.writelines(convert)
+                            print(fayl)
+                    return Response({'Original Text':  f"http://127.0.0.1:8000/{last_text}", 'Latin to Cyrillic': convert})
+            except KeyError:  # KeyError
+                return Response({'result': "Siz konvert qilishda adashdingiz!"})
+
+        return Response(last_text)
