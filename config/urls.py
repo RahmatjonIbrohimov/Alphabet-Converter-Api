b8160f2ecb55e4a1e6514babbc395ce1abaef6f6
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-05 09:31:58 +0500

    added spectacular urls, swagger-ui, and redoc

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
