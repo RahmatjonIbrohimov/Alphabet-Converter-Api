84a7c7d8152da5d58b1762f9125d8c891ce8657f
Rahmatjon <rahmatjonibragimov17@gmail.com>
2024-01-05 09:12:47 +0500

    changed, name, toLatin -> to_Latin

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class ConvertTextModel(models.Model):
     text = models.TextField()
+    to = models.CharField(choices=[('to_Latin', 'to_Latin'), ('to_Cyrillic', 'to_Cyrillic')], max_length=12)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -12,6 +13,7 @@ class ConvertTextModel(models.Model):
 
 class ConvertFileModel(models.Model):
     file = models.FileField(upload_to='files/', blank=True)
+    to = models.CharField(choices=[('to_Latin', 'to_Latin'), ('to_Cyrillic', 'to_Cyrillic')], max_length=12)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
